83f731a1cd8a5c0ca94c738ff97140e68fe452a5
4strys <145552280+4strys@users.noreply.github.com>
2023-10-23 15:01:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ brew install postgresql
 brew install python3
 brew install pip3
 pip3 install -r requirements.txt --break-system-packages
-brew services start postgresql
-createdb ProtoRH_API
-psql -d ProtoRH_API -U yassah -a -f database_rh.psql
-brew services stop postgresql
+#brew services start postgresql
+#createdb ProtoRH_API
+#psql -d ProtoRH_API -U yassah -a -f database_rh.psql
+#brew services stop postgresql
